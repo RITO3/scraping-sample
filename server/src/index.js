fbb5d01464197640116cf434b6d14326744b8479
RITO3 <25099791+RITO3@users.noreply.github.com>
2020-03-09 22:38:07 +0900

    スペルミス.

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/assets/images', express.static('./assets/images'));
 app.use('/assets/css', express.static('./assets/css'));
-app.use(
-  '/assets/bootsrtrap',
-  express.static('./node_modules/bootstrap/dist'),
-);
+app.use('/assets/bootstrap', express.static('./node_modules/bootstrap/dist'));
 app.use('/assets/jquery', express.static('./node_modules/jquery/dist'));
 app.get('/', function(req, res) {
   res.render('index');
